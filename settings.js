3e018969eaca20df222850ab4aae009c2228f253
Danny Milsom <danny.milsom@cgi.com>
2014-10-30 13:14:08 +0000

    Added 1/2 points and ? values to the default point options in settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 var settings = {};
 
-settings.points = [0, 1, 2, 3, 5, 8, 13, 20, 40, 100];
+settings.points = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100, '?'];
 settings.validEmails = [];
 settings.adminEmail = 'danny.milsom@cgi.com';
 
